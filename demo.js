93378d561eb5a040a98800f91452aab9a0be0f93
wujianping <1043375451@qq.com>
2017-07-20 18:44:26 +0800

    crawler.js

--- demo.js
+++ demo.js
@@ -1,23 +1,44 @@
 const fs = require('fs');
-const list = require('./list.js');
+// const list = require('./list.js');
 
-fs.readFile('films.txt', (err, data) => {
-    if (err) {
-        console.log(err)
-    } else {
-        let movies = data.toString().split('\n');
+// fs.readFile('films.txt', (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         let movies = data.toString().split('\n');
 
-        for (let i = 0; i < movies.length; i++) {
-            list.append(movies[i].trim());
-        }
-         
-          
-        function displayList(list) {
-            for (list.front(); list.currPos() < list.length(); list.next()) {
-                 console.log(list.getElement());
+//         for (let i = 0; i < movies.length; i++) {
+//             list.append(movies[i].trim());
+//         }
+
+
+//         function displayList(list) {
+//             for (list.front(); list.currPos() < list.length(); list.next()) {
+//                  console.log(list.getElement());
+//             }
+//         }
+//         // displayList(list)
+//         console.log(list)
+//     }
+// });
+
+
+const readFile = (fileName) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, (err, data) => {
+            if (err) {
+                reject(err)
             }
-        }
-        // displayList(list)
-        console.log(list)
-    }
-});
+            resolve(data)
+        })
+    })
+}
+
+async function read() {
+    let f1 = await readFile('./films.txt')
+    let f2 = await readFile('./list.js')
+    console.log(f1.toString())
+    console.log(f2.toString())
+}
+
+read() 
